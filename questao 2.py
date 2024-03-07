7098ea25f19db3173f974c6daba858991a680965
Wellington silva <143335396+wellingtonsw09@users.noreply.github.com>
2024-03-07 10:31:10 -0300

    RESPOSTAS

---
+++ questao 2.py
@@ -0,0 +1,31 @@
+print('_'*30)
+
+print('Sequência de Fibonacci')
+
+print('_'*30)
+
+n = int(input('Insira um numero para gerar a sequência de Fibonnaci: '))
+
+t1 = 0
+
+t2 = 1
+
+print('~'*30)
+
+print('{} -> {}'.format(t1, t2), end = '')
+
+cont = 3
+
+while cont <= n:
+
+   t3 = t1 + t2
+
+   print('-> {}'.format(t3), end = '')
+
+   t1 = t2
+
+   t2 = t3
+
+   cont += 1
+
+print('-> FIM')
---
+++ questao1.py
@@ -0,0 +1,13 @@
+Indice = 13
+
+Soma = 0
+
+K = 0
+
+while K < Indice:
+
+   K = K + 1
+
+   Soma = Soma + K
+
+print (Soma)
